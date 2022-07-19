241857036ac87771198959fe90278994a30c09ce
Nitro <nitrodev0@gmail.com>
2022-07-19 12:39:10 +0900

    Upload base

---
+++ AutoUpdate.py
@@ -0,0 +1,19 @@
+# AutoUpdate.py
+#
+# Copyright 2022 Nitro. All Rights Reserved.
+#
+# Developer: Nitro (admin@nitrostudio.dev)
+# Blog: https://blog.nitrostudio.dev
+# Discord: Nitro#1781
+# GitHub: https://github.com/Nitro1231/AutoUpdate
+#
+# Version: 1.0.0
+# Last Modified: N/A
+#
+# This project is licensed under the GNU Affero General Public License v3.0;
+# you may not use this file except in compliance with the License.
+
+
+class AutoUpdate():
+    def __init__(self) -> None:
+        pass
---
+++ Events.py
@@ -0,0 +1,66 @@
+# Events.py
+#
+# Copyright 2022 Nitro. All Rights Reserved.
+#
+# Developer: Nitro (admin@nitrostudio.dev)
+# Blog: https://blog.nitrostudio.dev
+# Discord: Nitro#1781
+# GitHub: https://github.com/Nitro1231/AutoUpdate
+#
+# Version: 1.0.0
+# Last Modified: N/A
+#
+# This project is licensed under the GNU Affero General Public License v3.0;
+# you may not use this file except in compliance with the License.
+
+
+def download_file():
+    pass
+
+def checksum():
+    pass
+
+def unzip_file():
+    pass
+
+def create_dir():
+    pass
+
+def copy_dir():
+    pass
+
+def move_dir():
+    pass
+
+def read_file():
+    pass
+
+def write_file():
+    pass
+
+def copy_file():
+    pass
+
+def move_file():
+    pass
+
+def remove_file():
+    pass
+
+def read_json():
+    pass
+
+def write_json():
+    pass
+
+def update_json():
+    pass
+
+def check_json():
+    pass
+
+def edit_json_value():
+    pass
+
+def add_json_item():
+    pass
