01351fb4a4ab3944ea50e5b73fc78a5dd23eca19
Nitro <nitrodev0@gmail.com>
2022-07-24 21:53:48 +0900

    Update Events.py

--- Events.py
+++ Events.py
@@ -14,53 +14,88 @@
 # you may not use this file except in compliance with the License.
 
 
-def download_file():
-    pass
+import os
+import json
+import shutil
+import hashlib
+import zipfile
+import requests
 
-def checksum():
-    pass
 
-def unzip_file():
-    pass
+def download_file(url: str, path: str) -> None:
+    req = requests.get(url)
+    with open(path, 'wb') as f:
+        f.write(req.content)
 
-def create_dir():
-    pass
+def checksum(path: str) -> str:
+    with open(path, 'rb') as f:
+        data = f.read()
+        hash_value = hashlib.sha256(data).hexdigest();
+    return hash_value
 
-def copy_dir():
-    pass
+def unzip_file(zip_path: str, target_path: str) -> None:
+    with zipfile.ZipFile(zip_path, 'r') as f:
+        f.extractall(target_path)
 
-def move_dir():
-    pass
 
-def read_file():
-    pass
+def create_dir(path: str) -> None:
+    os.makedirs(path)
 
-def write_file():
-    pass
+def copy_dir(src_path: str, dest_path: str) -> None:
+    shutil.copytree(src_path, dest_path)
 
-def copy_file():
-    pass
+def delete_dir(path: str) -> None:
+    os.rmdir(path)
 
-def move_file():
-    pass
 
-def remove_file():
-    pass
+def read_file(path: str) -> str:
+    with open(path, 'r', encoding='utf-8') as f:
+        return f.read()
 
-def read_json():
-    pass
+def write_file(path: str, content: str) -> None:
+    with open(path, 'w', encoding='utf-8') as f:
+        f.write(content)
 
-def write_json():
-    pass
+def copy_file(src_path: str, dest_path: str) -> None:
+    shutil.copyfile(src_path, dest_path)
 
-def update_json():
-    pass
+def delete_file(path: str) -> None:
+    os.remove(path)
+
+
+def move_item(src_path: str, dest_path: str) -> None:
+    shutil.move(src_path, dest_path)
+
+def rename_item(src_path: str, dest_path: str) -> None:
+    os.rename(src_path, dest_path)
+
+
+def read_json(path: str) -> dict:
+    with open(path, 'r', encoding='utf-8') as f:
+        return json.load(f)
+
+def write_json(path: str, data: dict) -> None:
+    with open(path, 'w', encoding='utf-8') as f:
+        json.dump(data, f, indent=4)
+
+def update_json(org_data: dict, new_data: dict) -> dict:
+    for k, v in new_data.items():
+        if v == dict:
+            if k in org_data:
+                org_data[k] = update_json(org_data[k], v)
+            else:
+                org_data[k] = v
+        else:
+            org_data[k] = v
+    return org_data
 
 def check_json():
     pass
 
-def edit_json_value():
-    pass
+def update_json_value(data: dict, key: str, value: any) -> dict:
+    data[key] = value
+    return data
 
-def add_json_item():
-    pass
+def delete_json_item(data: dict, key: str) -> dict:
+    del data[key]
+    return data
